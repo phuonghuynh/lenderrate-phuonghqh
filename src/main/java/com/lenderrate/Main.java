d95152153eaf666db2ff84508bb87de397750975
phuonghqh <phuonghqh@gmail.com>
2017-02-17 12:33:03 +0700

    Remove redundant imports

--- src/main/java/com/lenderrate/Main.java
+++ src/main/java/com/lenderrate/Main.java
@@ -1,10 +1,9 @@
 package com.lenderrate;
 
-import java.lang.reflect.Array;
-import java.util.*;
-import java.util.stream.Collector;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Main {
 
